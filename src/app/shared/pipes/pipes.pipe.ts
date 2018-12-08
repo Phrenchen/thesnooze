d1bc4ee56d72ea176b0acd069446519740426a68
Kevin Stähler <phrenchen@gmail.com>
2018-12-08 17:40:30 +0100

    css fix. deleted unused pipe.

--- src/app/shared/pipes/pipes.pipe.ts
+++ src/app/shared/pipes/pipes.pipe.ts
@@ -1,16 +1,6 @@
-import { Pipe, PipeTransform } from '@angular/core';
+import { Pipe, PipeTransform, NgModule } from '@angular/core';
 import { DomSanitizer } from '@angular/platform-browser';
 
-@Pipe({
-  name: 'pipes'
-})
-export class PipesPipe implements PipeTransform {
-
-  transform(value: any, args?: any): any {
-    return null;
-  }
-
-}
 
 @Pipe({ name: 'safe' })
 export class SafePipe implements PipeTransform {
